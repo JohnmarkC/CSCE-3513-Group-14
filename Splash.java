3ce7923b0ba0211938464c07d00a9cbbd4e4c2e4
abrasher123 <158104078+abrasher123@users.noreply.github.com>
2024-02-18 23:06:54 -0600

    Update Splash.java

--- Splash.java
+++ Splash.java
@@ -6,17 +6,16 @@ import java.awt.Image;
 public class Splash extends JFrame
 {
 
-	public Splash()
-	{
-		//JFrame splash = new JFrame();
-
+	public Splash(){
 		// Set some window properties
 		this.setTitle("Photon");
 		this.setSize(900, 650);
 		this.setFocusable(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
-		
+    	}
+
+    public void create(){
         // Load and add the image
         ImageIcon imageIcon = new ImageIcon("logo.jpg");
         Image originalImage = imageIcon.getImage();
@@ -26,18 +25,9 @@ public class Splash extends JFrame
         this.add(imageLabel);
 
         this.setVisible(true);
-
-        // Wait for 3 seconds
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-	}
-
-	public static void main(String[] args)
-	{
+    }
+    
+	public static void main(String[] args){
 		Splash g = new Splash();
 	}
 	
