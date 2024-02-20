a90827b542123524ce1d880ea1932e893d845e65
abrasher123 <158104078+abrasher123@users.noreply.github.com>
2024-02-19 19:05:14 -0600

    Update Client.java
    Added splashscreen.create();

--- Client.java
+++ Client.java
@@ -1,10 +1,17 @@
 public class Client {
     player_entry_view playerscreen = new player_entry_view();
-    Splash splash = new Splash();
+    Splash splashscreen = new Splash();
     SupaBaseIntegration playerbase = new SupaBaseIntegration();
     UDP upd = new UDP();
 
     public void screen(){
+        splashscreen.create();
+        try {
+            // Wait 3 seconds
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         playerscreen.create();
     }
 
