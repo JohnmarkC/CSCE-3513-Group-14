be2617d44845b4c6c84b113716f0b8a6619b02e2
JohnmarkC <93932297+JohnmarkC@users.noreply.github.com>
2024-02-25 13:51:20 -0600

    Adding a script file to compile and run the project on the ubuntu VM

---
+++ run.sh
@@ -0,0 +1,9 @@
+javac UDP.java
+javac TextPrompt.java
+javac SupaBaseIntegration.java
+javac Splash.java
+javac PlayerEntryScreen.java
+javac Model.java
+javac Controller.java
+javac Client.java
+java Client
