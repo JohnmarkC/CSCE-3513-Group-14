f7c4ee8ba103e9c25d7de62d241f0bb10b3c4225
TLucas10 <158004080+TLucas10@users.noreply.github.com>
2024-02-25 20:30:02 -0800

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,17 +1,21 @@
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import static java.lang.System.*;
 
 
-class Controller implements KeyListener
+class Controller implements KeyListener, MouseListener
 {
     player_entry_view view;
     Model model;
+    SupaBaseIntegration data;
 
     //Constructor
     Controller(Model m)
     {
         model = m;
+        data = new SupaBaseIntegration();
     }
 
     void setView(player_entry_view v)
@@ -24,16 +28,36 @@ class Controller implements KeyListener
         switch (e.getKeyCode())
         {
             case KeyEvent.VK_ENTER: // Enter key is pressed
-                model.search();
-                //Send user names and IDs to the database then prompt for equipment id
+                int id = 0;
+                String name = "";
+                String result = "";
+                boolean changename = false;
+                for(int i = 0; i < 30; i++)
+                {
+                    name = view.RedTeam[i].getText();
+                    id = Integer.parseInt(view.RedTeam[i+15].getText());
+                    result = data.playerData(id, name, false);
+                    view.RedTeam[i].setText(result);
+                }
+                for(int i = 0; i < 30; i++)
+                {
+                    name = view.GreenTeam[i].getText();
+                    id = Integer.parseInt(view.RedTeam[i+15].getText());
+                    result = data.playerData(id, name, false);
+                    view.RedTeam[i].setText(result);
+                }
                 break;
             case KeyEvent.VK_F5: //F5 key pressed
-                model.start();            
-                //begin the games
+                view.frame.getContentPane().removeAll();
+                view.frame.repaint();
+                //create the game
                 break;
             case KeyEvent.VK_F12: // F12 key pressed
-                model.clear();
-                //clear all entries on the player entry screen
+                for(int i = 0; i < 45; i++)
+                {
+                    view.RedTeam[i].setText("");
+                    view.GreenTeam[i].setText("");
+                }
                 break;
         }
     }
@@ -43,6 +67,23 @@ class Controller implements KeyListener
 
     public void keyTyped(KeyEvent e)
     {   }
+
+    public void mousePressed(MouseEvent e)
+    {   }
+
+    public void mouseExited(MouseEvent e)
+    {   }
+
+    public void mouseEntered(MouseEvent e)
+    {   }
+
+    public void mouseReleased(MouseEvent e)
+    {   }
+
+    public void mouseClicked(MouseEvent e)
+    {
+        view.frame.requestFocusInWindow();
+    }
 
     void update()
     {
