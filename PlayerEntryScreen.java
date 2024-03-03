e92fba11d73ff303d7065675d169f6b5b800729e
JohnmarkC <93932297+JohnmarkC@users.noreply.github.com>
2024-03-02 18:12:11 -0600

    Update PlayerEntryScreen.java
    Fixed the full screen function and got the new labels aligned

--- PlayerEntryScreen.java
+++ PlayerEntryScreen.java
@@ -1,13 +1,8 @@
-import javax.swing.JPanel;
-import javax.swing.JComponent;
-import java.awt.Graphics;
 import java.awt.Font;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-import java.io.File;
 import javax.swing.*;
 import java.awt.Color;
-import javax.swing.JFrame;
 import java.awt.Toolkit;
 import java.awt.Image;
 
@@ -37,16 +32,13 @@ class player_entry_view extends JPanel
         //create a JFrame on which to create the player entry screen
         frame.setTitle("Entry Terminal");
         frame.getContentPane().setBackground(Color.BLACK);
-	//I'm not sure why this line causes issues with the ubuntu VM but it does so I commented it out
-        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-	    frame.setSize(width, height);
+        frame.setSize(java.awt.Toolkit.getDefaultToolkit().getScreenSize());
         frame.setFocusable(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
         frame.addKeyListener(controller);
         frame.addMouseListener(controller);
         frame.setResizable(false);
-
 
         // send key events to the controller
 	this.addKeyListener(c);
@@ -56,29 +48,6 @@ class player_entry_view extends JPanel
 	this.Red_team = new String[45];
         this.Green_team = new String[45];
         this.game = new String[60];  
-    }
-    public static void main(String args[])
-    {
-        Model model = new Model();
-        Controller controller = new Controller(model);
-        player_entry_view p = new player_entry_view(controller, model);
-        p.create();
-        while(true)
-		{
-			controller.update();
-			model.update();
-			//view.repaint(); // Indirectly calls View.paintComponent
-			Toolkit.getDefaultToolkit().sync(); // Updates screen
-
-			// Go to sleep for a brief moment
-			try
-			{
-				Thread.sleep(25);
-			} catch(Exception e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-		}
     }
 
     public void create_splash()
@@ -100,6 +69,8 @@ class player_entry_view extends JPanel
        this.frame.repaint();
         JPanel Redpanel = new JPanel();
         JPanel Greenpanel = new JPanel();
+        Redpanel.setLayout(null);
+        Greenpanel.setLayout(null);
         Redpanel.setBackground(new Color(122, 37, 34));
         Greenpanel.setBackground(new Color(34, 122, 66));
         frame.add(Redpanel);
@@ -121,40 +92,87 @@ class player_entry_view extends JPanel
         JLabel g = new JLabel("Green Team");
         r.setForeground(Color.WHITE);
         g.setForeground(Color.WHITE);
-        r.setFont(new Font("calibri", Font.BOLD, 20));
-        g.setFont(new Font("calibri", Font.BOLD, 20));
+        r.setFont(new Font("calibri", Font.BOLD, 25));
+        g.setFont(new Font("calibri", Font.BOLD, 25));
         r.setVerticalAlignment(JLabel.TOP);
         g.setVerticalAlignment(JLabel.TOP);
         r.setHorizontalAlignment(JLabel.CENTER);
         g.setHorizontalAlignment(JLabel.CENTER);
-        Redpanel.add(r);
-        Greenpanel.add(g);
 
-        //headers for each text field column
+        JPanel redTitle = new JPanel();
+        redTitle.setOpaque(false);
+        redTitle.setBounds(0, 5, width/2 -75, height/2);
+        redTitle.add(r);
+        Redpanel.add(redTitle);
+
+        JPanel greenTitle = new JPanel();
+        greenTitle.setOpaque(false);
+        greenTitle.setBounds(0, 5, width/2 -75, height/2);
+        greenTitle.add(g);
+        Greenpanel.add(greenTitle);
+        //create the labels indicating what each column of text fields represents
 
         JLabel redHeader = new JLabel("Player Name");
+        redHeader.setForeground(Color.WHITE);
+        redHeader.setFont(new Font("calibri", Font.BOLD, 18));
         redHeader.setHorizontalAlignment(JLabel.CENTER);
-        Redpanel.add(redHeader);
-    
+        JPanel transparent_panel1 = new JPanel();
+        transparent_panel1.setOpaque(false);
+        transparent_panel1.setBounds(25, 40, 225, 25);
+        transparent_panel1.add(redHeader);
+        Redpanel.add(transparent_panel1);
+
         JLabel redHeader2 = new JLabel("Player ID");
+        redHeader2.setForeground(Color.WHITE);
+        redHeader2.setFont(new Font("calibri", Font.BOLD, 18));
         redHeader2.setHorizontalAlignment(JLabel.CENTER);
-        Redpanel.add(redHeader2);
+        JPanel transparent_panel2 = new JPanel();
+        transparent_panel2.setOpaque(false);
+        transparent_panel2.setBounds(275, 40, 225/2, 25);
+        transparent_panel2.add(redHeader2);
+        Redpanel.add(transparent_panel2);
     
         JLabel redHeader3 = new JLabel("Equipment ID");
+        redHeader3.setForeground(Color.WHITE);
+        redHeader3.setFont(new Font("calibri", Font.BOLD, 18));
         redHeader3.setHorizontalAlignment(JLabel.CENTER);
-        Redpanel.add(redHeader3);
-    
+        JPanel transparent_panel3 = new JPanel();
+        transparent_panel3.setOpaque(false);
+        transparent_panel3.setBounds(225/2+300, 40, 225/2, 25);
+        transparent_panel3.add(redHeader3);
+        Redpanel.add(transparent_panel3);
+
         JLabel greenHeader = new JLabel("Player Name");
+        greenHeader.setForeground(Color.WHITE);
+        greenHeader.setFont(new Font("calibri", Font.BOLD, 18));
         greenHeader.setHorizontalAlignment(JLabel.CENTER);
-        Greenpanel.add(greenHeader);
-    
+        JPanel transparent_panel4 = new JPanel();
+        transparent_panel4.setOpaque(false);
+        transparent_panel4.setBounds(25, 40, 225, 25);
+        transparent_panel4.add(greenHeader);
+        Greenpanel.add(transparent_panel4);
+
         JLabel greenHeader2 = new JLabel("Player ID");
+        greenHeader2.setForeground(Color.WHITE);
+        greenHeader2.setFont(new Font("calibri", Font.BOLD, 18));
         greenHeader2.setHorizontalAlignment(JLabel.CENTER);
-        Greenpanel.add(greenHeader2);
+        JPanel transparent_panel5 = new JPanel();
+        transparent_panel5.setOpaque(false);
+        transparent_panel5.setBounds(275, 40, 225/2, 25);
+        transparent_panel5.add(greenHeader2);
+        Greenpanel.add(transparent_panel5);
     
-        JLabel greenHeader3 = new JLabel("Equipment ID");
-        greenHeader3.setHorizontalAlignment(JLabel.CENTER);
-        Greenpanel.add(greenHeader3);
+        JLabel GreenHeader3 = new JLabel("Equipment ID");
+        GreenHeader3.setForeground(Color.WHITE);
+        GreenHeader3.setFont(new Font("calibri", Font.BOLD, 18));
+        GreenHeader3.setHorizontalAlignment(JLabel.CENTER);
+        JPanel transparent_panel6 = new JPanel();
+        transparent_panel6.setOpaque(false);
+        transparent_panel6.setBounds(225/2+300, 40, 225/2, 25);
+        transparent_panel6.add(GreenHeader3);
+        Greenpanel.add(transparent_panel6);
+    
+        
     
         this.frame.setVisible(true);
 
@@ -164,7 +182,7 @@ class player_entry_view extends JPanel
     RedTeam = new JTextField[45];
     GreenTeam = new JTextField[45];
         Nx = 25;
-        y = 50;
+        y = 75;
         Nwidth = 225;
         Nheight = 25;
         for(int i = 0; i<15; i++){
@@ -183,18 +201,9 @@ class player_entry_view extends JPanel
             Greenpanel.add(GreenTeam[i]);
             tp2.changeAlpha(0.5f);
             GreenTeam[i].setBounds(Nx,y,Nwidth,Nheight);
-            
-            //old text fields
-            //RedTeam[i] = new JTextField("Enter your name");
-            //RedTeam[i].setBackground(Color.WHITE);
-            //Redpanel.add(RedTeam[i]);
-            //RedTeam[i].setBounds(Nx,y,Nwidth,Nheight);
-            // GreenTeam[i] = new JTextField("Enter your name");
-            // Greenpanel.add(GreenTeam[i]);
-            // GreenTeam[i].setBounds(Nx,y,Nwidth,Nheight);
             y += 35;
         }
-        y = 50;
+        y = 75;
         Ix = 275;
         for(int i = 15; i<30; i++){
             RedTeam[i] = new JTextField(10);
@@ -210,17 +219,9 @@ class player_entry_view extends JPanel
             Greenpanel.add(GreenTeam[i]);
             tp2.changeAlpha(0.5f);
             GreenTeam[i].setBounds(Ix,y,Nwidth/2,Nheight);
-
-            // RedTeam[i] = new JTextField("Enter your id");
-            // RedTeam[i].setBackground(Color.WHITE);
-            // Redpanel.add(RedTeam[i]);
-            // RedTeam[i].setBounds(Ix,y,Nwidth/2,Nheight);
-            // GreenTeam[i] = new JTextField("Enter your id");
-            // Greenpanel.add(GreenTeam[i]);
-            // GreenTeam[i].setBounds(Ix,y,Nwidth/2,Nheight);
             y += 35;
         }
-        y = 50;
+        y = 75;
         int Eidx = Nwidth/2+Ix+25;
         for(int i = 30; i<45; i++){
             RedTeam[i] = new JTextField(10);
@@ -236,14 +237,6 @@ class player_entry_view extends JPanel
             Greenpanel.add(GreenTeam[i]);
             tp2.changeAlpha(0.5f);
             GreenTeam[i].setBounds(Eidx,y,Nwidth/2,Nheight);
-            
-            // RedTeam[i] = new JTextField("Enter Equipment id");
-            // RedTeam[i].setBackground(Color.WHITE);
-            // Redpanel.add(RedTeam[i]);
-            // RedTeam[i].setBounds(Eidx,y,Nwidth/2,Nheight);
-            // GreenTeam[i] = new JTextField("Enter Equipment id");
-            // Greenpanel.add(GreenTeam[i]);
-            // GreenTeam[i].setBounds(Eidx,y,Nwidth/2,Nheight);
             y += 35;
         }
     }
