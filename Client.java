8505d9cb87e2e0f516cc590f87801869339e519e
JohnmarkC <93932297+JohnmarkC@users.noreply.github.com>
2024-04-15 13:11:22 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,8 +7,35 @@ public class Client {
 
     public void screen(){
         model.launch();
+        while(!playerscreen.players_loaded)
+        {
+            try
+            {
+                Thread.sleep(5000);
+            }
+            catch
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
+        startUpdates();    
     }
 
+    public void startUpdates()
+    {
+        while(true)
+        {
+            playerscreen.sort_players();
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Client client = new Client();
